8ab0959f7dbf3ae90f21d182baf92de800820e5e
Illia <ilya@sheremetov.com>
2020-08-27 13:17:51 +0200

    Updates

---
+++ precache-manifest.997e2ceb19e90433742c59d41fc3e636.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "069ddf9c4f3fc0ed59bd2544c53f9931",
+    "url": "/archimedus/index.html"
+  },
+  {
+    "revision": "670a8fe0d9a064be8a11",
+    "url": "/archimedus/static/css/main.c263f1ec.chunk.css"
+  },
+  {
+    "revision": "1530cc1312216b2f120c",
+    "url": "/archimedus/static/js/2.7427119e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/archimedus/static/js/2.7427119e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "670a8fe0d9a064be8a11",
+    "url": "/archimedus/static/js/main.b9afb9e1.chunk.js"
+  },
+  {
+    "revision": "dfd659c8194a754c323a",
+    "url": "/archimedus/static/js/runtime-main.dfce2e83.js"
+  },
+  {
+    "revision": "5d98e7267d69342cd7f38bd5125dac03",
+    "url": "/archimedus/static/media/logo2.5d98e726.png"
+  }
+]);
